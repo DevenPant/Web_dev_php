a6c53a03b734aca76d3a0492d5a16dd9bca5c151
DevenPant <149228076+DevenPant@users.noreply.github.com>
2023-10-28 13:25:12 +0530

    Update script-12.php

--- TASK-2/script-12.php
+++ TASK-2/script-12.php
@@ -1,7 +1,7 @@
 <!-- Write a PHP script to concatenate two strings and display the combined result. -->
 <?php
-$string1 = "AVANI ";
-$string2 = "VIRAMGAMA";
+$string1 = "DEVEN ";
+$string2 = "PANT";
 
 $combined = $string1 . $string2;
 
