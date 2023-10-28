9868bcd67e852746f68c4d61297f540975eb0d46
DevenPant <149228076+DevenPant@users.noreply.github.com>
2023-10-28 13:32:32 +0530

    Update Script_54.php

--- TASK-3/Script_54.php
+++ TASK-3/Script_54.php
@@ -5,23 +5,23 @@ the array_column() function. */
 $a = array(
     array(
       'id' => 101,
-      'first_name' => 'avani',
-      'last_name' => 'viramgama',
+      'first_name' => 'DEVEN',
+      'last_name' => 'PANT',
     ),
     array(
       'id' => 4767,
-      'first_name' => 'jay',
-      'last_name' => 'kanani',
+      'first_name' => 'JAY',
+      'last_name' => 'KANANI',
     ),
     array(
       'id' => 3809,
-      'first_name' => 'manth',
-      'last_name' => 'Patel',
+      'first_name' => 'SHREY',
+      'last_name' => 'PATEL',
     )
   );
   
   $last_names = array_column($a, 'last_name');
   print_r($last_names);
 
-//Output:Array ( [0] => viramgama [1] => kanani [2] => Patel )
+//Output:Array ( [0] => PANT [1] => KANANI [2] => PATEL )
 ?>
